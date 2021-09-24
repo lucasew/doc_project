983e6107ec8f135453fda16f87afecb5f83e5b7e
lucasew <lucas59356@gmail.com>
2021-09-24 13:45:10 -0300

    repl: disable parser import
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmd/doclua/main.go
+++ cmd/doclua/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	lua_new "github.com/lucasew/doc_project/lua/new"
 	utils_lua "github.com/lucasew/doc_project/lua/utils"
-	_ "github.com/lucasew/doc_project/lua/stdlib/parser"
 	_ "github.com/lucasew/doc_project/lua/stdlib/base/time"
 	lua "github.com/yuin/gopher-lua"
 )
